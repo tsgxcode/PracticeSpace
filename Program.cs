2259310db552e14ca8f782afa02cf11eeef2631c
TSG <TSG@TSGAC>
2020-08-24 05:45:23 -0700

    8.24.2020 Refactored a and b strings to interpolation to make code cleaner.

--- Program.cs
+++ Program.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.IO;
 
 namespace PracticeSpace
 {
     class Program
     {
+        public static string FullText { get; set; }
 
         public static void Main()
         {
@@ -12,6 +14,9 @@ namespace PracticeSpace
             //Basic if/ else. A simple on/off switch, depending on the operator. the == truns the switch on.
             string a = "Off";
             string b = "On";
+            string fullText = $"{a}.{b}.";
+            File.AppendAllText(@"C:\Users\TSG\source\repos\PracticeSpace\Data.txt", FullText + Environment.NewLine);
+
             if (a + b != "On or Off? ")
             {
                 Console.WriteLine("Off");
